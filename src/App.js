17a974dd24c46f59d467a3ff4d0e76ec95b3e1a0
Caren Agudelo <karenagudelo11@gmail.com>
2023-09-25 00:07:19 -0500

    finally project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
+import TareaFormulario from "./componentes/TareaFormulario";
+import ListaDeTareas from "./componentes/ListaDeTareas";
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={freeCodeCampLogo}
+          className="freecodecamp-logo"
+          alt="Logo de freeCodeCamp"
+        />
+      </div>
+      <div className="tareas-lista-principal">
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
